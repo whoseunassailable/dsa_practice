8c14debef7a74c64dae5f407a3d72cb9252259da
Rohan Bhande <rohanbhande11@gmail.com>
2025-01-16 12:04:36 -0600

    first commit

---
+++ two_sum_1.py
@@ -0,0 +1,13 @@
+array = [7, 12, 18, 25]
+
+# Find the locker with the number 18.
+whereIs18 = array.index(18)
+
+print(f"Location of 18 element is : {whereIs18}")
+
+# Add the number 30 to the end of the array.
+array.append(30)
+
+array.insert(2, 99)
+
+print(array)
